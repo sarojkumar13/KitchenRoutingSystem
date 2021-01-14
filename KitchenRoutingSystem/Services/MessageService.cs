7cc5811609f958cec3beda49a6b421ba7df2aecd
SAROJ KUMAR <sarojkumar_13@yahoo.co.in>
2021-01-14 11:13:25 -0500

    Tabified the code

--- KitchenRoutingSystem/Services/MessageService.cs
+++ KitchenRoutingSystem/Services/MessageService.cs
@@ -11,33 +11,34 @@ using System.Threading.Tasks;
 
 namespace KitchenRoutingSystem.Services
 {
-    public class MessageService : IMessageService
-    {
-        private readonly IConfiguration _config;
-        private readonly ILogger<MessageService> _logger;
+	public class MessageService : IMessageService
+	{
+		private readonly IConfiguration _config;
+		private readonly ILogger<MessageService> _logger;
 
-        public MessageService(IConfiguration config, ILogger<MessageService> logger)
-        {
-            _config = config;
-            _logger = logger;
+		public MessageService(IConfiguration config, ILogger<MessageService> logger)
+		{
+			_config = config;
+			_logger = logger;
 
-        }
+		}
 
-        public async Task SendOrderToKitchenAsync<T>(T order, string topicName, string filter)
-        {
-            var topicClient = new TopicClient(_config.GetConnectionString("AzureServiceBus"), topicName);
-            string orderQueue = JsonSerializer.Serialize(order);
-            Message message = new Message(Encoding.UTF8.GetBytes(orderQueue));
-            message.Label = filter;
-            try
-            {
-                await topicClient.SendAsync(message);
-            }
+		public async Task SendOrderToKitchenAsync<T>(T order, string topicName, string filter)
+		{
+			try
+			{
+				var topicClient = new TopicClient(_config.GetConnectionString("AzureServiceBus"), topicName);
+				string orderQueue = JsonSerializer.Serialize(order);
+				Message message = new Message(Encoding.UTF8.GetBytes(orderQueue));
+				message.Label = filter;
 
-            catch (Exception e)
-            {
-                _logger.LogError(e.Message);
-            }
-        }
-    }
+				await topicClient.SendAsync(message);
+			}
+
+			catch (Exception e)
+			{
+				_logger.LogError(e.Message);
+			}
+		}
+	}
 }
